0a927a71664c4783708c8d661c188d22ac45ac82
digitalfarmer <adesbsp@gmail.com>
2021-07-16 10:55:54 +0700

    create input siswa with flask message sukses

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -11,4 +11,10 @@ class SiswaController extends Controller
         $data_siswa =Siswa::all();
         return view('siswa.index',['data_siswa'=>$data_siswa]);
     }
+
+    public function create(Request $request){
+
+        Siswa::create($request->all());
+        return redirect('/siswa')->with('sukses','Data Berhasil di inpur');
+    }
 }
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -13,6 +13,13 @@
 <body>
 
 <div class="container">
+    @if(session('sukses'))
+    <div class="alert alert-success" role="alert">
+       {{session('sukses')}}
+        <button type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
     <div class="row">
     <div class="col-6">
         <!-- Button trigger modal -->
@@ -60,19 +67,48 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form>
+                <form action="/siswa/create" method="POST">
+                    {{ csrf_field() }}
                     <div class="mb-3">
                         <label for="nama_depan" class="form-label">Nama Depan</label>
-                        <input type="text" class="form-control" id="nama_depan" aria-describedby="emailHelp">
+                        <input name="nama_depan" type="text" class="form-control" id="nama_depan" aria-describedby="emailHelp" placeholder="Nama Depan">
                         {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>--}}
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                    <div class="mb-3">
+                        <label for="nama_belakang" class="form-label">Nama Blekang</label>
+                        <input name="nama_belakang" type="text" class="form-control" id="nama_belakang" aria-describedby="emailHelp" placeholder="Nama Belakang">
+                        {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>--}}
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="jenis_kelamin" class="form-label">Pilih Jenis Kelamin</label>
+                        <select name="jenis_kelamin" class="form-select" aria-label="Default select example">
+                            <option value="L">Laki-Laki</option>
+                            <option value="P">Perempuan</option>
+
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="agama" class="form-label">Agama</label>
+                        <input name="agama" type="text" class="form-control" id="agama" aria-describedby="emailHelp" placeholder="Agama / Kepercayaan">
+                        {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>--}}
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="alamat" class="form-label">Alamat</label>
+                        <textarea name="alamat" class="form-control" rows="3" id="alamat"></textarea>
+                        {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>--}}
+                    </div>
+
+
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
